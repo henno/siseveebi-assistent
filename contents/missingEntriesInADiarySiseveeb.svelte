5b1fac9bc4ecf3d4df9bec58e26f2e0e272fe593
ArnoldITA20 <arnold.grosberg@voco.ee>
2023-06-01 14:36:47 +0300

    WIP

--- contents/missingEntriesInADiarySiseveeb.svelte
+++ contents/missingEntriesInADiarySiseveeb.svelte
@@ -5,6 +5,7 @@
         PlasmoMountShadowHost,
         PlasmoGetStyle
     } from "plasmo"
+
     export const getStyle: PlasmoGetStyle = () => {
         const style = document.createElement("style")
         style.textContent = `
@@ -26,12 +27,6 @@
     }
 
 
-
-
-
-
-
-
     export const config: PlasmoCSConfig = {
         matches: ["https://siseveeb.voco.ee/kutseope/oppetoo/paevik/taitmine*"]
     }
@@ -51,7 +46,6 @@
     }
 
 
-
 </script>
 
 <script>
@@ -60,6 +54,40 @@
         {day: "2021-09-01", hours: 2, type: "S"},
     ]
 
+    import { onMount } from 'svelte';
+
+    // Get the id from a page string using regex to get the id of the user
+    function getId(fetchId) {
+        // example data: $(document).ready(function(){page_data.previously_logged_user_id=28243;
+        // match the id between the = and the ;
+        return fetchId.match(/page_data.previously_logged_user_id=(.*?);/)[1]
+    }
+
+    onMount(async () => {
+        const fetchId = await fetch("https://siseveeb.voco.ee/toidu_menuu").then(res => res.text())
+        // Do something with the fetched data
+        const idMatch = getId(fetchId)
+        console.log(idMatch)
+        localStorage.setItem("id", idMatch);
+        // Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'local')
+        // chrome.storage.local.get(['id'], (result) => {
+
+        console.log('3')
+
+        // Define a function to send a message to the background script
+        function sendMessageToBackground(message) {
+            chrome.runtime.sendMessage(message, (response) => {
+                // Handle the response from the background script if needed
+                console.log('Response from background:', response);
+            });
+        }
+
+        // Example usage of sending a message to the background script
+        const message = {
+            data: fetchId,
+        };
+        sendMessageToBackground(message);
+    });
 </script>
 
 <div>
